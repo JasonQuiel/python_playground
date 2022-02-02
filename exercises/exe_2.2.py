0543c342858fc879d902852cf6e0bce6a33f0c57
jason <jason-quiel@live.nmit.ac.nz>
2022-02-02 16:34:03 +1300

    while loops example

---
+++ exercises/exe_2.2.py
@@ -0,0 +1,27 @@
+# creating a while loop that uses break and continue.
+
+while True:
+    int_value = input("Put in a number and rember to say Please to stop the loop: ")
+    if int_value == "Please":
+        break
+    number = int(int_value)
+    if number % 2 == 0:
+        continue
+print(number, "The square is: ", number * number)
+
+
+# Another while loop,
+
+number_sequ = [1, 3, 5, 7, 9, 11]
+# Start checking at index 0 in the list
+position = 0
+# While the position is less than the lenght of the list, keep checking
+while position < len(number_sequ):
+# number_check holds the values to compaire as it goes through the list
+    number_check = number_sequ[position]
+    if number_check % 2 == 0:
+        print("Found an even number!", number_check)
+        break
+    position += 1 # Moves the index up by 1 for each loop until the end.
+else:
+    print("No even number was found")
