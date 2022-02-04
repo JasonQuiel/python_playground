dce639bca644f41f71614bc9dbe125d765f571ca
jason <jason-quiel@live.nmit.ac.nz>
2022-02-04 15:19:28 +1300

    free code campe exp

---
+++ exercises/exe_2.4.py
@@ -0,0 +1,21 @@
+
+from ast import Break
+
+
+add = 0
+total = 0
+
+while True:
+    number_input = input("Enter a number: ")
+    if number_input == 'done':
+        break
+    try:
+        value = float(number_input)
+    except:
+        print("Not a valid number")
+        continue
+
+    total = total + value
+    add = add + 1
+
+print(total, add, total/add)
