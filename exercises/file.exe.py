b07d63cca4bb100a0c94b7a1fad8d9070008316c
jason <jason-quiel@live.nmit.ac.nz>
2022-02-17 15:46:25 +1300

    working with split.

--- exercises/file.exe.py
+++ exercises/file.exe.py
@@ -15,3 +15,14 @@ for new in fcdc:
     if new.startswith('From'):
         print(new)
 
+# prints the days of the week using the position of a list using from as a start point.
+days = open('mbox-short.txt')
+
+for search in days:
+    remove_n = search.rstrip() # removes the new line \n
+    x = remove_n.split() #breakes the string into a list.
+    if len(x) < 1:
+        continue
+    if x[0] != 'From':
+        continue
+    print(x[2])
